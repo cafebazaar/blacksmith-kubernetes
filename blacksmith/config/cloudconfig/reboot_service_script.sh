6ed7b2d979fb4c22f818905cf9ba46edb9107815
Touhid Arastu <touhid.arastu@gmail.com>
2016-07-18 14:31:53 +0430

    pass etcd configuration to the templates

--- blacksmith/config/cloudconfig/reboot_service_script.sh
+++ blacksmith/config/cloudconfig/reboot_service_script.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
-IFS=. read node_name cluster_name << "<<<" >> $(hostname)
+node_name=$(echo $(hostname) | cut -f1 -d.)
+cluster_name=$(echo $(hostname) | cut -f2 -d.)
 
 state_key=/$cluster_name/machines/$node_name/state
 
 while true; do
-	state = $(etcdctl watch $state_key)
+	state=$(etcdctl watch $state_key)
 	if [[ $state =~ ^"init-" ]]; then
 		sudo systemctl reboot
 	fi
