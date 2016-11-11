1a7ad38491530ce8261f75e0d0c50f592567bf17
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-11 10:24:53 +0330

    Changed workspace updater listen url to blacksmith advertising url

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -2,9 +2,9 @@ CURRENT_HASH=$(/usr/bin/etcdctl get cafecluster/workspace-hash);
 BOOT_HASH=$(cat /var/lib/blacksmith/workspaces/workspace-hash);
 if [ "$CURRENT_HASH" != "$BOOTHASH" ];
     then
-        /usr/bin/coreos-cloudinit --from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
+        /usr/bin/coreos-cloudinit --from-url http://<<.IP>>:8000/t/cc/<<.Mac>>;
         /usr/bin/locksmithctl reboot;
 fi;
 /usr/bin/etcdctl watch /cafecluster/workspace-hash; 
-/usr/bin/coreos-cloudinit --from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
+/usr/bin/coreos-cloudinit --from-url http://<<.IP>>:8000/t/cc/<<.Mac>>;
 /usr/bin/watch /usr/bin/locksmithctl reboot;
