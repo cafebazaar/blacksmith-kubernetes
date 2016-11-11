561bcbc26ce9d989a7aa84d480629877428ce4cf
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-11 11:56:04 +0330

    Changed update watch url

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -1,10 +1,10 @@
 CURRENT_HASH=$(/usr/bin/etcdctl get cafecluster/workspace-hash);
 BOOT_HASH=$(cat /var/lib/blacksmith/workspaces/workspace-hash);
-if [ "$CURRENT_HASH" != "$BOOTHASH" ];
+if [[ "$CURRENT_HASH" != "$BOOT_HASH" ]];
     then
-        /usr/bin/coreos-cloudinit --from-url http://<<.IP>>:8000/t/cc/<<.Mac>>;
+        /usr/bin/coreos-cloudinit --from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
         /usr/bin/locksmithctl reboot;
 fi;
 /usr/bin/etcdctl watch /cafecluster/workspace-hash; 
-/usr/bin/coreos-cloudinit --from-url http://<<.IP>>:8000/t/cc/<<.Mac>>;
+/usr/bin/coreos-cloudinit --from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
 /usr/bin/watch /usr/bin/locksmithctl reboot;
