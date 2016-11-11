3ef40eef14d94cfdcf3667a0bf155ec444e383af
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-11 07:59:55 +0330

    Insecure docker registry option

--- config/cloudconfig/install-bootstrapper.sh
+++ config/cloudconfig/install-bootstrapper.sh
@@ -2,7 +2,7 @@
 
 ## Installing Blacksmith Docker
 if [[ $(docker -H unix:///var/run/early-docker.sock inspect blacksmith) == "[]" ]]; then
-  VOLUME_ARGS="-v /var/lib/blacksmith/workspaces:/workspaces"
+  VOLUME_ARGS="-v /var/lib/blacksmith/workspaces:/workspace"
   ARGS="-etcd http://<< (cluster_variable "bootstrapper1_ip") >>:2379,http://<< (cluster_variable "bootstrapper2_ip") >>:2379,http://<< (cluster_variable "bootstrapper3_ip") >>:2379 -if $1 -cluster-name << (cluster_variable "cluster_name") >> -lease-start << (cluster_variable "internal_network_workers_start") >> -lease-range <<(cluster_variable "internal_network_workers_limit")>> -dns << (cluster_variable "external_dns") >> -file-server << (cluster_variable "file_server") >> -workspace-repo git@git.cafebazaar.ir:ali.javadi/blacksmith-kubernetes.git"
   docker -H unix:///var/run/early-docker.sock pull << (cluster_variable "blacksmith_image") >>
   docker -H unix:///var/run/early-docker.sock run --name blacksmith --restart=always -d --net=host $VOLUME_ARGS << (cluster_variable "blacksmith_image") >> $ARGS
