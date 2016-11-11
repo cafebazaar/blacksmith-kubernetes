ed3d168a1a3ef401449a519f9e531c34edbced76
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-11 10:01:49 +0330

    Fixed multiple bugs in workspace updater service

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -1,10 +1,10 @@
 CURRENT_HASH=$(/usr/bin/etcdctl get cafecluster/workspace-hash);
 BOOT_HASH=$(cat /var/lib/blacksmith/workspaces/workspace-hash);
-if [ "$CURRENT_HASH" == "$BOOTHASH" ];
+if [ "$CURRENT_HASH" != "$BOOTHASH" ];
     then
-        /usr/bin/coreos-cloudinit --from-url http://<<.WebServerAddr>>/t/cc/<<.Mac>>;
+        /usr/bin/coreos-cloudinit --from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
         /usr/bin/locksmithctl reboot;
 fi;
 /usr/bin/etcdctl watch /cafecluster/workspace-hash; 
-/usr/bin/coreos-cloudinit --from-url http://<<.WebServerAddr>>/t/cc/<<.Mac>>;
+/usr/bin/coreos-cloudinit --from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
 /usr/bin/locksmithctl reboot;
