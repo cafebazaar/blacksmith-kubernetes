881470e5e78fc82ddee2ee1b5eb447bb22be71ca
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-14 17:52:44 +0330

    Fixed bug in updater

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -1,16 +1,6 @@
-CURRENT_HASH=$(/usr/bin/etcdctl get cafecluster/workspace-hash);
-BOOT_HASH=$(cat /var/lib/blacksmith/workspaces/workspace-hash);
-if [[ "$CURRENT_HASH" != "$BOOT_HASH" ]];
-    then
-        docker -H unix:///var/run/early-docker.sock pull  << (cluster_variable "blacksmith_image") >>;
-        docker -H unix:///var/run/early-docker.sock restart blacksmith
-        /usr/bin/etcdctl watch /cafecluster/workspace-commit-hash
-        /usr/bin/coreos-cloudinit --from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
-        /usr/bin/locksmithctl reboot;
-fi;
-/usr/bin/etcdctl watch /cafecluster/workspace-hash > /var/lib/blacksmith/workspaces/workspace-hash
+/usr/bin/etcdctl watch /cafecluster/workspace-hash;
 docker -H unix:///var/run/early-docker.sock pull  << (cluster_variable "blacksmith_image") >>; 
 docker -H unix:///var/run/early-docker.sock restart blacksmith;
-/usr/bin/etcdctl watch /cafecluster/workspace-commit-hash
+/usr/bin/etcdctl watch /cafecluster/workspace-commit-hash;
 /usr/bin/coreos-cloudinit --from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
 while true; do locksmithctl reboot; sleep 2; done
