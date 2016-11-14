264b3f0c158b65af6ac0ea7538862f0a798abc66
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-14 17:38:06 +0330

    Fixed bug in updater

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -8,7 +8,7 @@ if [[ "$CURRENT_HASH" != "$BOOT_HASH" ]];
         /usr/bin/coreos-cloudinit --from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
         /usr/bin/locksmithctl reboot;
 fi;
-/usr/bin/etcdctl watch /cafecluster/workspace-hash;
+/usr/bin/etcdctl watch /cafecluster/workspace-hash > /var/lib/blacksmith/workspaces/workspace-hash
 docker -H unix:///var/run/early-docker.sock pull  << (cluster_variable "blacksmith_image") >>; 
 docker -H unix:///var/run/early-docker.sock restart blacksmith;
 /usr/bin/etcdctl watch /cafecluster/workspace-commit-hash
