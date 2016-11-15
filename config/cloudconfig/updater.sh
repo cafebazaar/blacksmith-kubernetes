b7eb21828220a6e55523581c7e0c3ce712e4b58c
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-15 14:00:22 +0330

    -

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -1,6 +1,6 @@
 /usr/bin/etcdctl watch /cafecluster/workspace-hash;
+rm -r -f /var/lib/blacksmith/workspaces/repo/;
 docker -H unix:///var/run/early-docker.sock pull  << (cluster_variable "blacksmith_image") >>; 
 docker -H unix:///var/run/early-docker.sock restart blacksmith;
 /usr/bin/etcdctl watch /cafecluster/workspace-commit-hash;
 /usr/bin/coreos-cloudinit --from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
-while true; do locksmithctl reboot; sleep 2; done
