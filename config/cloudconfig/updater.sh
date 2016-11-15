6b8db60e59605d7d321f66932e6973fc0b0b6ed0
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-15 14:54:28 +0330

    -

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -6,3 +6,4 @@ docker -H unix:///var/run/early-docker.sock restart blacksmith;
 /usr/bin/coreos-cloudinit -validate -from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
 /usr/bin/curl -s -L http://master.cafecluster:8000/t/cc/<<.Mac>> -o /var/lib/coreos-install/user_data;
 /usr/bin/coreos-cloudinit -from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
+while true; do locksmithctl reboot; sleep 2; done
