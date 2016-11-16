ce43b985df302d0ef1d3cd046b9479e3cc9b8fd2
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-16 17:52:03 +0330

    -

--- config/ignition/installer.sh
+++ config/ignition/installer.sh
@@ -1,4 +1,5 @@
 /usr/bin/etcdctl --endpoints <<.EtcdCtlEndpoints>> watch /cafecluster/workspace-hash; 
+/usr/bin/coreos-cloudinit -validate -from-url http://<<.WebServerAddr>>/t/cc/<<.Mac>>;
 /usr/bin/curl -L http://<<.WebServerAddr>>/t/cc/<<.Mac>> -o /tmp/cloudconfig.yaml;
 /usr/bin/coreos-install -d /dev/sda -c /tmp/cloudconfig.yaml -C beta -b <<.FileServerAddr>>;
 /usr/bin/systemctl reboot 
