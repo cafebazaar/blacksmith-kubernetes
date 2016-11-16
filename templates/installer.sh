698df84ff98bc742b353c155a1a2e67af89c44da
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-16 08:57:20 +0330

    -

--- templates/installer.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/etcdctl --endpoints http://<<.BlacksmithIP>>:2379 watch /cafecluster/workspace-hash; 
-/usr/bin/curl -L http://<<.WebServerAddr>>/t/cc/<<.Mac>> -o /tmp/cloudconfig.yaml;
-/usr/bin/coreos-install -d /dev/sda -c /tmp/cloudconfig.yaml -C beta -b http://<<.FileServerAddr>>;
-/usr/bin/systemctl reboot
