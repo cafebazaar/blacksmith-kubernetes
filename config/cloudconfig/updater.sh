4a413dea8ea27a741f31c453605aef1ab56fdaff
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-16 07:58:21 +0330

    -

--- config/cloudconfig/updater.sh
+++ config/cloudconfig/updater.sh
@@ -1,5 +1,6 @@
 /usr/bin/etcdctl watch /cafecluster/workspace-hash;
 docker -H unix:///var/run/early-docker.sock restart blacksmith;
+/usr/bin/etcdctl watch /cafecluster/workspace-commit-hash;
 /usr/bin/coreos-cloudinit -validate -from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
 /usr/bin/curl -s -L http://master.cafecluster:8000/t/cc/<<.Mac>> -o /var/lib/coreos-install/user_data;
 /usr/bin/coreos-cloudinit -from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
