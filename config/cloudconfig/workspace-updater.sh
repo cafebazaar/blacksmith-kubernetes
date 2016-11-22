8dc11bf67e30973c071f2550567f2ff8d17f3ac2
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-22 19:18:29 +0330

    -

--- config/cloudconfig/workspace-updater.sh
+++ config/cloudconfig/workspace-updater.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+
+<< with $ip := (machine_variable "_machine/ip") >>
+
 /usr/bin/etcdctl watch /cafecluster/machines/`echo '<<.Mac>>' | sed  s/://g`/workspace-revision;
 /usr/bin/coreos-cloudinit -validate -from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
 /usr/bin/curl -s -L http://127.0.0.1:8000/t/cc/<<.Mac>> -o /var/lib/coreos-install/user_data;
 /usr/bin/coreos-cloudinit -from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
 while true; do locksmithctl reboot; sleep 2; done
+
+<< end >>
