37e246c828ac28ff79b13ca46d3cac440504443c
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-22 19:28:11 +0330

    -

--- config/cloudconfig/workspace-updater.sh
+++ config/cloudconfig/workspace-updater.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-
-<< with $ip := (machine_variable "_machine/ip") >>
-
+$LOCALIP=$(ifconfig << (machine_varaible "internal_interface_name") >>| sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 /usr/bin/etcdctl watch /cafecluster/machines/`echo '<<.Mac>>' | sed  s/://g`/workspace-revision;
-/usr/bin/coreos-cloudinit -validate -from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
-/usr/bin/curl -s -L http://127.0.0.1:8000/t/cc/<<.Mac>> -o /var/lib/coreos-install/user_data;
-/usr/bin/coreos-cloudinit -from-url http://127.0.0.1:8000/t/cc/<<.Mac>>;
+/usr/bin/coreos-cloudinit -validate -from-url http://$LOCALIP:8000/t/cc/<<.Mac>>;
+/usr/bin/curl -s -L http://$LOCALIP:8000/t/cc/<<.Mac>> -o /var/lib/coreos-install/user_data;
+/usr/bin/coreos-cloudinit -from-url http://$LOCALIP:8000/t/cc/<<.Mac>>;
 while true; do locksmithctl reboot; sleep 2; done
-
-<< end >>
