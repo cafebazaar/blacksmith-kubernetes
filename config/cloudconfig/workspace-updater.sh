1743ccf8554ebdbcc61af45435d584ecf3fe5665
Ali Javadi <ali.javadi@cafebazaar.ir>
2016-11-28 19:49:33 +0330

    -

--- config/cloudconfig/workspace-updater.sh
+++ config/cloudconfig/workspace-updater.sh
@@ -4,4 +4,4 @@ set -e
 /usr/bin/coreos-cloudinit -validate -from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
 /usr/bin/curl -s -L http://master.cafecluster:8000/t/cc/<<.Mac>> -o /var/lib/coreos-install/user_data;
 /usr/bin/coreos-cloudinit -from-url http://master.cafecluster:8000/t/cc/<<.Mac>>;
-while true; do locksmithctl reboot; sleep 2; done
+while true; do locksmithctl reboot || true; sleep 2; done
